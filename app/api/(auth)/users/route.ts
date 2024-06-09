df4e4e256f1685836e0a59f62b0a057d528c9c34
Herlea Marius <mariusherlea@gmail.com>
2024-06-09 11:48:13 +0300

    add post

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -14,3 +14,20 @@ export const GET = async () => {
     });
   }
 };
+
+export const POST = async (request: Request) => {
+  try {
+    const body = await request.json();
+    await connect();
+    const newUser = new User(body);
+    await newUser.save();
+    return new NextResponse(
+      JSON.stringify({ message: "User was created", user: newUser }),
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return new NextResponse("Error in creating user" + error.message, {
+      status: 500,
+    });
+  }
+};
