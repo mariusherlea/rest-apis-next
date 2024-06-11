5cb8355ea5117c50e0036c88821327a7268fe347
Herlea Marius <mariusherlea@gmail.com>
2024-06-11 09:03:53 +0300

    categories get and post

--- lib/modals/category.ts
+++ lib/modals/category.ts
@@ -1,9 +1,8 @@
 import { Schema, model, models } from "mongoose";
-import { title } from "process";
 
 const CategorySchema = new Schema(
   {
-    title: { type: "string", require: true },
+    title: { type: "string", required: true },
     user: { type: Schema.Types.ObjectId, ref: "User" },
   },
   {
